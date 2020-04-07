7f726ec86de9052ffab4db07ba681861a5ffcae6
qiannyqian <liang.huiqian@gmail.com>
2020-04-07 14:34:50 +0800

    refactored hooks out into custom hook, added UserList

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResources';
 
 const ResourceList = ({ resource }) => {
-  const [resources, setResources] = useState([]);
+  const resources = useResources(resource);
 
-  const fetchResource = async (resource) => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
-
-  useEffect(() => {
-    fetchResource(resource);
-  }, [resource]);
-
-  return <div>{resources.length}</div>;
+  return (
+    <ul>
+      {resources.map((record) => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ResourceList;
